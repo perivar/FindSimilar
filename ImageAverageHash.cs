6478c1b5298177a889848239bc277232460cc38e
Per Ivar Nerseth <perivar@nerseth.com>
2012-10-31 17:42:31 +0100

    Using the same BitCounter method in both PHash and Average Hash

---
+++ ImageAverageHash.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using CommonUtils;
+using Imghash;
+
+namespace Imghash
+{
+	// http://stackoverflow.com/questions/4240490/problems-with-dct-and-idct-algorithm-in-java
+	// http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html
+	// http://pastebin.com/Pj9d8jt5
+	// https://github.com/jforshee/ImageHashing/blob/master/ImageHashing/ImageHashing.cs
+	public class ImageAverageHash
+	{
+
+		private const bool doSaveimages = false;
+		
+		private static BitCounter bitCounter = new BitCounter(8);
+		
+		/// <summary>
+		/// Computes the average hash of an image according to the algorithm given by Dr. Neal Krawetz
+		/// on his blog: http://www.hackerfactor.com/blog/index.php?/archives/432-Looks-Like-It.html.
+		/// </summary>
+		/// <param name="image">The image to hash.</param>
+		/// <returns>The hash of the image.</returns>
+		public static ulong AverageHash(Image image)
+		{
+			int smallerSize = 8;
+			string fileSavePrefix = @"C:\Users\perivar.nerseth\Documents\My Projects\Code\FindSimilar\" + StringUtils.GetCurrentTimestamp();
+			
+			if (doSaveimages) image.Save(fileSavePrefix + "1-orig.png");
+			
+			Bitmap squeezedImage = CommonUtils.ImageUtils.Resize(image, smallerSize, smallerSize);
+			if (doSaveimages) squeezedImage.Save(fileSavePrefix + "2-squeezed.png");
+
+			uint averageValue = 0;
+			byte[] grayscaleByteArray = CommonUtils.ImageUtils.ImageToByteArray8BitGrayscale(squeezedImage, out averageValue);
+			if (doSaveimages) {
+				Image fromBinary = CommonUtils.ImageUtils.ByteArray8BitGrayscaleToImage(grayscaleByteArray, smallerSize, smallerSize);
+				fromBinary.Save(fileSavePrefix + "3-grayFromArray.png");
+
+				Bitmap grayscaleImage = CommonUtils.ImageUtils.MakeGrayscaleFastest(squeezedImage);
+				grayscaleImage.Save(fileSavePrefix + "4-grayscale.png");
+			}
+			
+			// Compute the hash: each bit is a pixel
+			// 1 = higher than average, 0 = lower than average
+			ulong hash = 0; // = Unsigned 64-bit integer
+			for(int i = 0; i < (squeezedImage.Width * squeezedImage.Height); i++)
+			{
+				//if(i % squeezedImage.Width == 0) {
+				//	Console.WriteLine();
+				//}
+
+				if(grayscaleByteArray[i] >= averageValue) {
+					hash |= (1UL << ((squeezedImage.Width * squeezedImage.Height - 1) - i));
+					//string binString = StringUtils.LongToBinaryString((long)hash);
+					//Console.Write(binString + " (" + bitCounter.CountOnesWithPrecomputation(hash) + ")\n");
+					//Console.Write(StringUtils.BinaryStringToHexString(binString)+"\n");
+
+					//	Console.Write(" ");
+					//} else {
+					//	Console.Write("#");
+				}
+			}
+			//Console.WriteLine();
+			//Console.WriteLine();
+
+			return hash;
+		}
+
+		/// <summary>
+		/// Computes the average hash of the image content in the given file.
+		/// </summary>
+		/// <param name="path">Path to the input file.</param>
+		/// <returns>The hash of the input file's image content.</returns>
+		public static ulong AverageHash(String path)
+		{
+			Bitmap bmp = new Bitmap(path);
+			return AverageHash(bmp);
+		}
+
+		/// <summary>
+		/// Returns a percentage-based similarity value between the two given hashes. The higher
+		/// the percentage, the closer the hashes are to being identical.
+		/// </summary>
+		/// <param name="hash1">The first hash.</param>
+		/// <param name="hash2">The second hash.</param>
+		/// <returns>The similarity percentage.</returns>
+		public static double Similarity(ulong hash1, ulong hash2)
+		{
+			int s1 = bitCounter.CountOnesWithPrecomputation(hash1 ^ hash2);
+			//int s2 = BitCounter.BitCount(hash1 ^ hash2);
+			//int s3 = BitCounter.Hamming(hash1, hash2);
+			
+			return ((64 - s1) * 100) / 64.0;
+			//return ((64 - bitCounter.CountOnesWithPrecomputation(hash1 ^ hash2)) * 100) / 64.0;
+		}
+
+		/// <summary>
+		/// Returns a percentage-based similarity value between the two given images. The higher
+		/// the percentage, the closer the images are to being identical.
+		/// </summary>
+		/// <param name="image1">The first image.</param>
+		/// <param name="image2">The second image.</param>
+		/// <returns>The similarity percentage.</returns>
+		public static double Similarity(Image image1, Image image2)
+		{
+			ulong hash1 = AverageHash(image1);
+			ulong hash2 = AverageHash(image2);
+			return Similarity(hash1, hash2);
+		}
+
+		/// <summary>
+		/// Returns a percentage-based similarity value between the image content of the two given
+		/// files. The higher the percentage, the closer the image contents are to being identical.
+		/// </summary>
+		/// <param name="image1">The first image file.</param>
+		/// <param name="image2">The second image file.</param>
+		/// <returns>The similarity percentage.</returns>
+		public static double Similarity(String path1, String path2)
+		{
+			ulong hash1 = AverageHash(path1);
+			ulong hash2 = AverageHash(path2);
+			return Similarity(hash1, hash2);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,34 +1,35 @@
 using System;
 using System.Drawing;
+using CommonUtils;
 
-public class Program
+namespace Imghash
 {
-
-	// http://stackoverflow.com/questions/7395836/converting-from-bitstring-to-integer
-	// http://www.dotnetperls.com/bitcount
-	// http://stackoverflow.com/questions/4777070/hamming-distance-on-binary-strings-in-sql
-	// http://www.fftw.org/doc/Real_002dto_002dReal-Transform-Kinds.html
-	
-	public static void Main(string[] args)
+	public class Program
 	{
-		//DCTTester();
-		HashTester(args);
-		
-		// TODO: remove this
-		Console.ReadKey();
-	}
-	
-	public static void DCTTester() {
-		int N = 2;
-		Random generator = new Random();
 
-		double[,] f = new double[N,N];
-		double[,] F = new double[N,N];
+		// http://stackoverflow.com/questions/7395836/converting-from-bitstring-to-integer
+		// http://stackoverflow.com/questions/4777070/hamming-distance-on-binary-strings-in-sql
+		// http://www.fftw.org/doc/Real_002dto_002dReal-Transform-Kinds.html
+		public static void Main(string[] args)
+		{
+			//DCTTester();
+			HashTester(args);
+			
+			// TODO: remove this
+			Console.ReadKey();
+		}
 		
-		// Generate random integers between 0 and 255
-		Console.WriteLine("Original values");
-		Console.WriteLine("-----------");
-		/*
+		public static void DCTTester() {
+			int N = 2;
+			Random generator = new Random();
+
+			double[,] f = new double[N,N];
+			double[,] F = new double[N,N];
+			
+			// Generate random integers between 0 and 255
+			Console.WriteLine("Original values");
+			Console.WriteLine("-----------");
+			/*
 		int @value;
 		for (int x=0;x<N;x++)
 		{
@@ -39,114 +40,121 @@ public class Program
 				Console.WriteLine(f[x,y]+" => f["+x+"]["+y+"]");
 			}
 		}
-		 */
-		
-		// force the values to compare
-		f = new double[,] { {54.0, 35.0}, {128.0, 185.0} };
-		for (int x=0;x<N;x++)
-		{
+			 */
+			
+			// force the values to compare
+			f = new double[,] { {54.0, 35.0}, {128.0, 185.0} };
+			for (int x=0;x<N;x++)
+			{
+				for (int y=0;y<N;y++)
+				{
+					Console.WriteLine(f[x,y]+" => f["+x+"]["+y+"]");
+				}
+			}
+			
+			//DCT dctApplied = new DCT(N);
+			//dctApplied.ForwardDCT(f, F);
+			DCT dctApplied = new DCT(N);
+			F = dctApplied.ApplyDCT(f);
+			
+			Console.WriteLine("\nFrom f to F");
+			Console.WriteLine("-----------");
+			for (int x=0;x<N;x++)
+			{
+				for (int y=0;y<N;y++)
+				{
+					try
+					{
+						Console.WriteLine(F[x,y]+" => F["+x+"]["+y+"]");
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e);
+					}
+				}
+			}
+
+			//dctApplied.InverseDCT(F, f);
+			f = dctApplied.ApplyIDCT(F);
+			
+			Console.WriteLine("\nBack to f");
+			Console.WriteLine("---------");
 			for (int y=0;y<N;y++)
 			{
-				Console.WriteLine(f[x,y]+" => f["+x+"]["+y+"]");
+				for (int z=0;z<N;z++)
+				{
+					Console.WriteLine(f[y,z]+" => f["+y+"]["+z+"]");
+				}
 			}
 		}
 		
-		//DCT dctApplied = new DCT(N);
-		//dctApplied.ForwardDCT(f, F);
-		DCT dctApplied = new DCT(N);
-		F = dctApplied.ApplyDCT(f);
-		
-		Console.WriteLine("\nFrom f to F");
-		Console.WriteLine("-----------");
-		for (int x=0;x<N;x++)
+		public static void HashTester(string[] argv)
 		{
-			for (int y=0;y<N;y++)
+			if(argv.Length == 1)
 			{
+				Bitmap theImage = new Bitmap(1,1);
+
 				try
 				{
-					Console.WriteLine(F[x,y]+" => F["+x+"]["+y+"]");
+					theImage = new Bitmap(argv[0]);
 				}
-				catch (Exception e)
+				catch(Exception)
 				{
-					Console.WriteLine(e);
+					Console.WriteLine("Couldn't open the image " + argv[0] + ".");
 				}
-			}
-		}
 
-		//dctApplied.InverseDCT(F, f);
-		f = dctApplied.ApplyIDCT(F);
-		
-		Console.WriteLine("\nBack to f");
-		Console.WriteLine("---------");
-		for (int y=0;y<N;y++)
-		{
-			for (int z=0;z<N;z++)
-			{
-				Console.WriteLine(f[y,z]+" => f["+y+"]["+z+"]");
+				Console.WriteLine(ImageAverageHash.AverageHash(theImage).ToString("x16") + "\t" + argv[0]);
 			}
-		}
-	}
-	
-	public static void HashTester(string[] argv)
-	{
-		if(argv.Length == 1)
-		{
-			Bitmap theImage = new Bitmap(1,1);
+			else if(argv.Length == 2)
+			{
+				Bitmap theImage = new Bitmap(1,1);
+				Bitmap theOtherImage  = new Bitmap(1,1);
 
-			try
-			{
-				theImage = new Bitmap(argv[0]);
-			}
-			catch(Exception)
-			{
-				Console.WriteLine("Couldn't open the image " + argv[0] + ".");
-			}
+				try
+				{
+					theImage = new Bitmap(argv[0]);
+				}
+				catch(Exception)
+				{
+					Console.WriteLine("Couldn't open the image " + argv[0] + ".");
+				}
+				try
+				{
+					theOtherImage = new Bitmap(argv[1]);
+				}
+				catch(Exception)
+				{
+					Console.WriteLine("Couldn't open the image " + argv[1] + ".");
+				}
 
-			Console.WriteLine(ImageHash.AverageHash(theImage).ToString("x16") + "\t" + argv[0]);
-		}
-		else if(argv.Length == 2)
-		{
-			Bitmap theImage = new Bitmap(1,1);
-			Bitmap theOtherImage  = new Bitmap(1,1);
+				
+				ulong hash1 = ImageAverageHash.AverageHash(theImage);
+				ulong hash2 = ImageAverageHash.AverageHash(theOtherImage);
+				
+				Console.WriteLine(hash1.ToString("x16") + "\t" + argv[0]);
+				Console.WriteLine(hash2.ToString("x16") + "\t" + argv[1]);
+				Console.WriteLine("Similarity: " + ImageAverageHash.Similarity(hash1, hash2) + "%");
+				Console.WriteLine("\n\n");
 
-			try
-			{
-				theImage = new Bitmap(argv[0]);
+				ImagePHash phash = new ImagePHash();
+				string hash1s = phash.GetHash(theImage);
+				string hash2s = phash.GetHash(theOtherImage);
+				Console.WriteLine(hash1s + "\t" + argv[0]);
+				Console.WriteLine(hash2s + "\t" + argv[1]);
+				Console.WriteLine("Similarity: " + phash.HammingDistance(hash1s, hash2s) + "");
+				
+				ulong hash1p = phash.PHash(theImage);
+				ulong hash2p = phash.PHash(theOtherImage);
+				Console.WriteLine(hash1p + "\t" + argv[0]);
+				Console.WriteLine(hash2p + "\t" + argv[1]);
+				Console.WriteLine("Similarity: " + BitCounter.Hamming(hash1p, hash2p) + "");
+				Console.WriteLine("Similarity: " + ImageAverageHash.Similarity(hash1p, hash2p) + "%");
 			}
-			catch(Exception)
+			else
 			{
-				Console.WriteLine("Couldn't open the image " + argv[0] + ".");
+				Console.WriteLine("To get the hash of an image: Imghash.exe <image name>");
+				Console.WriteLine("To compare two images: Imghash.exe <image 1> <image 2>");
 			}
-			try
-			{
-				theOtherImage = new Bitmap(argv[1]);
-			}
-			catch(Exception)
-			{
-				Console.WriteLine("Couldn't open the image " + argv[1] + ".");
-			}
-
-			/*
-			ulong hash1 = ImageHash.AverageHash(theImage);
-			ulong hash2 = ImageHash.AverageHash(theOtherImage);
-			
-			Console.WriteLine(hash1.ToString("x16") + "\t" + argv[0]);
-			Console.WriteLine(hash2.ToString("x16") + "\t" + argv[1]);
-			Console.WriteLine("Similarity: " + ImageHash.Similarity(hash1, hash2) + "%");
-			 */
-
-			ImagePHash phash = new ImagePHash();
-			string hash1s = phash.GetHash(theImage);
-			string hash2s = phash.GetHash(theOtherImage);
-			Console.WriteLine(hash1s + "\t" + argv[0]);
-			Console.WriteLine(hash2s + "\t" + argv[1]);
-			Console.WriteLine("Similarity: " + phash.HammingDistance(hash1s, hash2s) + "");
-		}
-		else
-		{
-			Console.WriteLine("To get the hash of an image: Imghash.exe <image name>");
-			Console.WriteLine("To compare two images: Imghash.exe <image 1> <image 2>");
 		}
 	}
-
 }
