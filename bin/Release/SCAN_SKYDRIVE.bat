5f618ef4154a944f89f2eac85f1bb60f01eeb7c7
Per Ivar Nerseth <perivar@nerseth.com>
2013-10-07 12:03:03 +0200

    Added bat files to simplify usage

---
+++ bin/Release/SCAN_SKYDRIVE.bat
@@ -0,0 +1,3 @@
+REM FindSimilar.exe -scandir="C:\Users\perivar.nerseth\SkyDrive\Audio\FL Studio Projects" -skipduration=30
+
+FindSimilar.exe -scandir="C:\Users\perivar.nerseth\SkyDrive\Audio" -skipduration=30
